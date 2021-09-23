da8a1a9d5b4df9dba0420c8eba9c96ea228c89b0
Luis Ortiz <66312625+luisortiz1211@users.noreply.github.com>
2021-09-22 21:49:19 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyDDP3MqNnv9Mc9VRII_tRJstmSSY53B5yk",
-    authDomain: "ionic-firebase-283ef.firebaseapp.com",
-    databaseURL: "https://ionic-firebase-283ef-default-rtdb.firebaseio.com",
-    projectId: "ionic-firebase-283ef",
-    storageBucket: "ionic-firebase-283ef.appspot.com",
-    messagingSenderId: "803941416644",
-    appId: "1:803941416644:web:38fb4f4517bf09da9e9391"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "h",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1"
   }
 
 };
